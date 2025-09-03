e793a2533bdeab2e799c066719d68aba00231397
UmaanMehraj <umaanmehraj5@gmail.com>
2025-09-03 09:57:06 +0530

    Sol:05_joinStrings

--- 05_joinStrings/joinStrings.js
+++ 05_joinStrings/joinStrings.js
@@ -8,7 +8,17 @@
 */
 
 // Add your code right below, good luck!
+const firstName = "Carlos";
+const lastName = "Stevenson";
+const fullName = firstName + " " + lastName;
 
+
+const birthYear = 1947;
+const thisYear = 1965;
+const age = thisYear - birthYear;
+
+
+const greeting = "Hello! My name is " + fullName + " and I am " + age + " years old.";
 
 
 
