c01bb9395fb5db757621f4abfb8810b9c433648d
UmaanMehraj <umaanmehraj5@gmail.com>
2025-09-14 08:48:58 +0530

    Sol: 06_repeatString

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,20 @@
-const repeatString = function() {
+const repeatString = function (str, num) {
+    let i = 1;
+    let arr = [];
+    if (num < 0) return "ERROR";
+    else {
+        while (i <= num) {
+            arr.push(str);
+            i++;
+        }
+    }
+    let newStr = arr.join('');
+    return newStr;
 
 };
+
+console.log(repeatString('hey', 3));
+
 
 // Do not edit below this line
 module.exports = repeatString;
