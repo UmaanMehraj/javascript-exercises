33d8e6a85188e9fdba6b3b28ef3fc6f9fd26be1f
UmaanMehraj <umaanmehraj5@gmail.com>
2025-09-14 13:22:18 +0530

    Sol: 11_tempConversion

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (tempInF) {
+  const tempinC = (tempInF - 32) * 5 / 9;
+  return parseFloat(tempinC.toFixed(1));
+
 };
 
-const convertToFahrenheit = function() {
+console.log(convertToCelsius(32));
+
+const convertToFahrenheit = function (tempInC) {
+  const tempInF = (tempInC * (9 / 5) + 32);
+  return parseFloat(tempInF.toFixed(1));
 };
 
 // Do not edit below this line
