049686341432d16917b3b92a3fdde6b3cd580402
UmaanMehraj <umaanmehraj5@gmail.com>
2025-09-14 09:00:21 +0530

    Sol: 07_reverseString

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function (str) {
+    let charArr = str.split('');
+    return charArr.reverse().join("");
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
