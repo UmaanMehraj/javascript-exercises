9c869f7d3f9f2b1e7c089480d9e80e585feaf789
UmaanMehraj <umaanmehraj5@gmail.com>
2025-09-15 20:59:02 +0530

    Sol: 09_sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,30 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+    let sum = 0;
+    if (a < 0 || b < 0) {
+        return "ERROR";
+    }
+    else {
+        if (Number.isInteger(a) && Number.isInteger(b)) {
+            if (a > b) {
+                for (let i = b; i <= a; i++) {
+                    sum += i;
+                }
+            }
+            else if (a < b) {
+                for (let i = a; i <= b; i++) {
+                    sum += i;
+                }
+            }
+        }
+        else {
+            return "ERROR";
+        }
+
+    }
+    return sum;
 
 };
+console.log(sumAll(1, 2));
 
 // Do not edit below this line
 module.exports = sumAll;
