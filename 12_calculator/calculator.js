c5791bb6be5334a822f49acb03b5f4af9f4f947a
UmaanMehraj <umaanmehraj5@gmail.com>
2025-09-28 15:04:49 +0530

    sol: 12_calculator

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
-};
-
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
+const add = function (a, b) {
+  return a + b
 
 };
 
-const power = function() {
-	
+const subtract = function (a, b) {
+  return a - b
+
 };
 
-const factorial = function() {
-	
+const sum = function (arr) {
+  const sum = arr.reduce((acc, num) => acc + num, 0)
+  return sum
+
+};
+
+const multiply = function (arr) {
+  const product = arr.reduce((acc, num) => acc * num, 1)
+  return product
+};
+
+const power = function (a, b) {
+  return a ** b
+
+};
+
+const factorial = function (a) {
+  if (a === 1 || a === 0) return 1;
+  else if (a < 0) return 'ERROR'
+  else {
+    return a * factorial(a - 1)
+  }
+
 };
 
 // Do not edit below this line
