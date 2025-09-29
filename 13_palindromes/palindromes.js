58a4ea598139f0b24284abc0c11f1d896e8046f8
UmaanMehraj <umaanmehraj5@gmail.com>
2025-09-29 20:11:40 +0530

    sol: Palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const alphanum = "abcdefghijklmnopqrstuvwxyz0123456789"
 
+    const cleanedString = str
+        .toLowerCase()
+        .split('')
+        .filter(char => alphanum.includes(char))
+        .join('')
+
+    const revStr = cleanedString.split('').reverse().join('')
+
+    return cleanedString === revStr
 };
 
 // Do not edit below this line
