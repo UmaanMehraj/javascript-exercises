95896eec56ffb4a8b9ac60ea91f02e04b597f8a9
UmaanMehraj <umaanmehraj5@gmail.com>
2025-10-01 08:17:45 +0530

    sol: 14_fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function (terms) {
+    let count
+    if (typeof terms != 'number') {
+        count = parseInt(terms)
+    }
+    else {
+        count = terms
+    }
 
+    if (count < 0) return 'OOPS'
+    else if (count === 0) return 0
+
+    let next = 1
+    let prev = 0
+
+    for (let i = 2; i <= count; i++) {
+        let current = next + prev
+        prev = next
+        next = current
+    }
+
+    return next
 };
 
 // Do not edit below this line
